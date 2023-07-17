3efddd30c01b40facc9399f03b03956572eabd51
Paula Ohashi <paulaohashi@gmail.comgit config --global user.name Paula>
2023-07-16 23:39:05 -0300

    feat aprofundamento-knex part

--- src/types.ts
+++ src/types.ts
@@ -13,3 +13,20 @@ export type TProduct = {
     description:string
     imageUrl:string
 }
+
+export interface Purchase {
+    purchaseId: string
+    buyerId: string
+    buyerName: string
+    buyerEmail: string
+    totalPrice: number
+    createdAt:string
+    products:Array<{
+      id: string
+      name: string
+      price: number
+      description: string
+      imageUrl: string
+      quantity: number
+    }>
+  }
